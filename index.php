1ee954144fb5d9feae160fb1096c8fe0d18daae8
Sreyem Sor <sreyem.sor@pnc.lan>
2019-03-01 09:11:11 +0700

    my website

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once "layout/header.php";
+require_once "layout/navbar.php";
+require_once "pages/content.php";
+require_once "layout/footer.php";
---
+++ pages/home.php
@@ -0,0 +1,9 @@
+<div class="container mt-4">
+    <div class="card bg-light shadow-lg">
+        <div class="card-header">Home page</div>
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur labore ut accusantium eveniet corporis deserunt omnis quidem saepe, tempora ullam. Natus aliquid facere voluptatum mollitia explicabo ab harum quisquam dolor.</p>
+        </div>
+
+    </div>
+</div>
